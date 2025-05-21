7b8e8e8d8e08de1dfb9c08a31b95734c4cba336f
Bekzodmirzaaliyev <bekzodmirzaaliyev27@gmail.com>
2025-05-21 10:57:28 +0500

    AS

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
-const cors = require('cors');
-const connectDB = require('./config/database');
-const userRouter = require('./routers/userRouter');
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+const userModel = require("./models/userModel");
+const connectDB = require("./config/database");
+const userRouter = require("./routers/userRouter");
 
 connectDB();
 
@@ -17,25 +18,24 @@ const server = http.createServer(app);
 app.use(cors());
 app.use(express.json());
 
-
-
 // API yo‘llar
 app.use("/api/users", userRouter);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173", // YOKI https://yourfrontenddomain.com
+    origin: ["http://localhost:5173", "http://localhost:5174"], // YOKI https://yourfrontenddomain.com
     methods: ["GET", "POST"],
-    credentials: true
-  }
+    credentials: true,
+  },
 });
 
+let onlineUsers = [];
 
-const onlineUsers = []
-
-io.on('connection', (socket) => {
-  console.log('Новый пользователь', socket.id);
-
+io.on("connection", async (socket) => {
+  console.log("Новый пользователь", socket.id);
+  const users = await userModel.find({});
+  onlineUsers = users
+  
   socket.on("user_joined", (user) => {
     console.log("User joined", user);
 
@@ -46,12 +46,21 @@ io.on('connection', (socket) => {
 
     io.emit("user_joined", user);
     io.emit("online_users", onlineUsers);
-  })
+
+    socket.on("disconnect", () => {
+      console.log("User left: ", user.phone);
+      const index = onlineUsers.findIndex((u) => u.phone === user.phone);
+      if (index !== -1) {
+        onlineUsers.splice(index, 1);
+      }
+      io.emit("online_users", onlineUsers);
+    });
+  });
 });
 
-app.use('/api/users', userRouter);
+app.use("/api/users", userRouter);
 
 // Serverni ishga tushirish
 server.listen(5000, () => {
-  console.log('🚀 Сервер запущен на http://localhost:5000');
+  console.log("🚀 Сервер запущен на http://localhost:5000");
 });
